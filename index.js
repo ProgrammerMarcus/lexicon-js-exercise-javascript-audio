1f31e70e6cca105b9781e49a6246b2b89edfe5bd
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-01-31 15:51:54 +0100

    css and responsivness improvement, better filtering, fake addtional tracks

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const db = [
+let db = [
     {
         title: "Boys, Girls, Toys & Words",
         artist: "Modern Pitch",
@@ -41,7 +41,11 @@ const db = [
         cover: "tracks/Say_Goodbye_-_VITNE.jpg",
         track: "tracks/Say_Goodbye_-_VITNE.mp3",
     },
-].sort((a, b) => a.title.localeCompare(b.title))
+].sort((a, b) => a.title.localeCompare(b.title));
+
+for (let i = 0; i < 5; i++) {
+    db = db.concat(db);
+}
 
 const state = {
     repeat: false,
@@ -62,17 +66,17 @@ document.querySelector(".buttons").addEventListener("click", (e) => {
     } else if (e.target.classList.contains("repeat")) {
         state.repeat = !state.repeat;
         if (state.repeat) {
-            e.target.classList.add("active")
+            e.target.classList.add("active");
         } else {
-            e.target.classList.remove("active")
+            e.target.classList.remove("active");
         }
     } else if (e.target.classList.contains("shuffle")) {
         toggleShuffle();
         shuffle();
         if (state.shuffle) {
-            e.target.classList.add("active")
+            e.target.classList.add("active");
         } else {
-            e.target.classList.remove("active")
+            e.target.classList.remove("active");
         }
     } else if (e.target.classList.contains("play")) {
         playPause();
@@ -89,13 +93,17 @@ audio.addEventListener("timeupdate", () => {
     document.querySelector("#progress").value = Math.round(
         (audio.currentTime / audio.duration) * 100
     );
-    document.querySelector("#progress").style["background-size"] = `${(audio.currentTime / audio.duration) * 100}% 100%`
+    document.querySelector("#progress").style["background-size"] = `${
+        (audio.currentTime / audio.duration) * 100
+    }% 100%`;
 });
 document.querySelector("#progress").addEventListener("change", (e) => {
     audio.currentTime = Math.round(
         (document.querySelector("#progress").value / 100) * audio.duration
     );
-    document.querySelector("#progress").style["background-size"] = `${(audio.currentTime / audio.duration) * 100}% 100%`
+    document.querySelector("#progress").style["background-size"] = `${
+        (audio.currentTime / audio.duration) * 100
+    }% 100%`;
 });
 
 function next() {
@@ -108,7 +116,7 @@ function next() {
     } else if (state.current !== state.songs.length - 1) {
         state.current += 1;
     }
-    play()
+    play();
     playPause(true);
 }
 
@@ -118,7 +126,7 @@ function previous() {
     } else if (state.current !== 0) {
         state.current = state.current - 1;
     }
-    play()
+    play();
     playPause(true);
 }
 
@@ -132,7 +140,7 @@ function shuffle() {
         state.current = 0;
     }
     play();
-    playPause(true)
+    playPause(true);
     generatePlaylist();
 }
 
@@ -144,27 +152,27 @@ function play() {
     audio.src = state.songs[state.current].track;
     document.querySelector(".info .cover").src = state.songs[state.current].cover;
     document.querySelector(".info .title").innerText = state.songs[state.current].title;
-    highlight()
+    highlight();
 }
 
 function playPause(bypass = false) {
     if (state.songs.length !== 0) {
         if (bypass) {
-            play()
+            play();
             audio.play();
-            document.querySelector(".pause").innerText = " pause_circle "
+            document.querySelector(".pause").innerText = " pause_circle ";
         } else if (audio.paused) {
             let temp = audio.currentTime;
-            audio.src = state.songs[state.current].track
+            audio.src = state.songs[state.current].track;
             audio.play();
             audio.currentTime = temp;
-            document.querySelector(".pause").innerText = " pause_circle "
+            document.querySelector(".pause").innerText = " pause_circle ";
         } else {
             audio.pause();
-            document.querySelector(".pause").innerText = " play_circle "
+            document.querySelector(".pause").innerText = " play_circle ";
         }
     } else {
-        audio.src = ""
+        audio.src = "";
     }
 }
 
@@ -175,114 +183,125 @@ function generatePlaylist() {
         let clone = document.querySelector("#template-track").cloneNode(true).content;
         clone.querySelector(".title").innerText = state.songs[t].title;
         clone.querySelector(".artist").innerText = `Artist: ${state.songs[t].artist}`;
-        clone.querySelector(".album").innerText =  `Album: ${state.songs[t].album}`;
+        clone.querySelector(".album").innerText = `Album: ${state.songs[t].album}`;
         clone.querySelector(".track").addEventListener("click", (e) => {
             if (state.current === t) {
                 playPause();
             } else {
                 state.current = t;
                 play();
-                playPause(true)
+                playPause(true);
             }
         });
         list.appendChild(clone);
     }
-    state.current = 0
+    state.current = 0;
     if (state.songs.length !== 0) {
         document.querySelector(".cover").src = state.songs[state.current].cover;
         document.querySelector(".info .title").innerText = state.songs[state.current].title;
     } else {
-        document.querySelector(".cover").src = "assets/cover.jpg"
+        document.querySelector(".cover").src = "assets/cover.jpg";
         document.querySelector(".info .title").innerText = "None";
     }
-    highlight()
+    highlight();
 }
 
 function highlight() {
     if (state.songs.length !== 0) {
-        let playing = document.querySelector(".playing")
+        let playing = document.querySelector(".playing");
         if (playing) {
-            playing.classList.remove("playing")
+            playing.classList.remove("playing");
         }
-        document.querySelectorAll(".playlist .track")[state.current].classList.add("playing")
+        document.querySelectorAll(".playlist .track")[state.current].classList.add("playing");
     }
 }
 
 document.querySelectorAll(".switch").forEach((s) => {
     s.addEventListener("click", (e) => {
         if (getComputedStyle(document.querySelector(".pop-up"))["display"] === "block") {
-            document.querySelector(".pop-up").style.display = "none"
-            return
+            document.querySelector(".pop-up").style.display = "none";
+            return;
         }
-        document.querySelector(".pop-up").style.display = "block"
+        document.querySelector(".pop-up").style.display = "block";
         for (c of document.querySelectorAll("#select .container")) {
-            c.remove()
+            c.remove();
         }
         if (e.currentTarget.classList.contains("tracks")) {
             for (t of db) {
-                let clone = document.querySelector("#template-selector").cloneNode(true).content
-                clone.querySelector("label span").insertAdjacentText('beforeend', t.title);
-                document.querySelector("#select").appendChild(clone)
+                let clone = document.querySelector("#template-selector").cloneNode(true).content;
+                clone.querySelector("label span").insertAdjacentText("beforeend", t.title);
+                document.querySelector("#select .flex").appendChild(clone);
             }
+            document.querySelector("#select h2").innerText = "Tracks";
         } else if (e.currentTarget.classList.contains("albums")) {
-            for (t of db) {
-                let clone = document.querySelector("#template-selector").cloneNode(true).content
-                clone.querySelector("label span").insertAdjacentText('beforeend', t.album);
-                document.querySelector("#select").appendChild(clone)
+            for (t of new Set(db.map((x) => x.album))) {
+                let clone = document.querySelector("#template-selector").cloneNode(true).content;
+                clone.querySelector("label span").insertAdjacentText("beforeend", t);
+                document.querySelector("#select .flex").appendChild(clone);
             }
+            document.querySelector("#select h2").innerText = "Albums";
         } else if (e.currentTarget.classList.contains("artists")) {
-            for (t of db) {
-                let clone = document.querySelector("#template-selector").cloneNode(true).content
-                clone.querySelector("label span").insertAdjacentText('beforeend', t.artist);
-                document.querySelector("#select").appendChild(clone)
+            for (t of new Set(db.map((x) => x.artist))) {
+                let clone = document.querySelector("#template-selector").cloneNode(true).content;
+                clone.querySelector("label span").insertAdjacentText("beforeend", t);
+                document.querySelector("#select .flex").appendChild(clone);
             }
+            document.querySelector("#select h2").innerText = "Artists";
         }
-
-    })
-})
+    });
+});
 
 /** Need to add proper filtering */
 document.querySelector("#select").addEventListener("submit", (e) => {
-    e.preventDefault()
-    let accepted = Array.from(e.currentTarget.querySelectorAll("input:checked")).map((c) => c.nextElementSibling.innerText)
-    state.songs = Array.from(db).filter((a) => accepted.includes(a.title))
-    generatePlaylist()
-    playPause(true)
-    document.querySelector(".pop-up").style.display = "none"
-})
+    e.preventDefault();
+    let accepted = Array.from(e.currentTarget.querySelectorAll("input:checked")).map(
+        (c) => c.nextElementSibling.innerText
+    );
+    let type = document.querySelector("#select h2").innerText;
+    if (type === "Tracks") {
+        state.songs = Array.from(db).filter((a) => accepted.includes(a.title));
+    } else if (type === "Artists") {
+        state.songs = Array.from(db).filter((a) => accepted.includes(a.artist));
+    } else if (type === "Albums") {
+        state.songs = Array.from(db).filter((a) => accepted.includes(a.album));
+    }
+    generatePlaylist();
+    playPause(true);
+    document.querySelector(".pop-up").style.display = "none";
+});
 
 function newTab(name = "") {
     if (state.playlists.length >= 9) {
-        return
+        return;
     }
-    let clone = document.querySelector("#template-tab").cloneNode(true).content.firstElementChild
-    let newPlaylist = []
-    state.playlists.push(newPlaylist)
+    let clone = document.querySelector("#template-tab").cloneNode(true).content.firstElementChild;
+    let newPlaylist = [];
+    state.playlists.push(newPlaylist);
     if (name !== "") {
-        clone.innerText = name
+        clone.innerText = name;
     } else {
-        clone.innerText = state.playlists.length
+        clone.innerText = state.playlists.length;
     }
-    clone.setAttribute("num", state.playlists.length - 1)
+    clone.setAttribute("num", state.playlists.length - 1);
     clone.addEventListener("click", (e) => {
-        document.querySelector(".tabs .selected").classList.remove("selected")
-        e.currentTarget.classList.add("selected")
-        state.playlists[state.playlist] = state.songs
-        state.playlist = e.currentTarget.getAttribute("num")
-        state.songs = state.playlists[e.currentTarget.getAttribute("num")]
-        generatePlaylist()
-        playPause(true)
-    })
-    document.querySelector(".tabs > .tab.new").insertAdjacentElement("beforebegin", clone)
-    return clone
+        document.querySelector(".tabs .selected").classList.remove("selected");
+        e.currentTarget.classList.add("selected");
+        state.playlists[state.playlist] = state.songs;
+        state.playlist = e.currentTarget.getAttribute("num");
+        state.songs = state.playlists[e.currentTarget.getAttribute("num")];
+        generatePlaylist();
+        playPause(true);
+    });
+    document.querySelector(".tabs > .tab.new").insertAdjacentElement("beforebegin", clone);
+    return clone;
 }
 
 document.querySelector(".tabs > .tab.new").addEventListener("click", () => {
-    newTab()
-})
+    newTab();
+});
 
 function init() {
-    newTab("Auto").classList.add("selected")
+    newTab("Auto").classList.add("selected");
     generatePlaylist();
 }
 
